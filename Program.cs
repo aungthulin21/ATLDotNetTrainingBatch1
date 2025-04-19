57ceacd98d39f1224afcd9ef72ce2f3a9af9ce78
Aung Thu Lin <aungthulin.atl.personal@gmail.com>
2025-04-19 14:56:26 +0630

    code update

--- Program.cs
+++ Program.cs
@@ -34,10 +34,110 @@
 //        Console.WriteLine("Invalid input");
 //        break;
 //}
+
+//for and foreach
 //for (int i = 0; i < 10; i++)
 //{
 //    Console.WriteLine("Before");
 //    Console.WriteLine(i);
 //    Console.WriteLine("After");
 //}
+//string[] str = { "frist", "second", "third" };
+//for (int i = 0; i < str.Length; i++)
+//{
+//    Console.WriteLine(str[i]);
+//}
+
+//foreach (string item in str)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//class and object
+//Resume resume=new Resume();
+//resume.Name = "Mg Mg";
+//resume.Age = 17;
+//var result=resume.Is18();
+//Console.WriteLine(resume.Name);
+//Console.WriteLine(resume.Age);
+//Console.WriteLine(result);
+
+//Resume resume=new Resume("Mg Mg",18);
+
+
+//public class Resume
+//{
+//    public Resume()
+//    {
+//        Name = "None";
+//    }
+
+//    public Resume(string name, int age)
+//    {
+//        Name= name;
+//        Age = age;
+//    }
+//    public string Name { get; set; }
+//    public int Age { get; set; }
+//    public bool Is18()
+//    {
+//        bool result= Age > 18;
+//        return result;
+//    }
+//}
+
+
+//Inheritance
+//Dog d = new Dog();
+//d.Eat();
+//d.Bark();
+//public class Animal
+//{
+//    public void Eat()
+//    {
+//        Console.WriteLine("Animal eat food");
+//    }
+//}
+//public class Dog : Animal
+//{
+//    public void Bark()
+//    {
+//        Console.WriteLine("Dog is barking");
+//    }
+//}
+
+//interface
+//public interface Iinterface
+//{
+//    void Create();
+//    void Read();
+//    void Update();
+//    void Delete();
+//}
+
+//public class Kpay : Iinterface
+//{
+//    public void Create()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Delete()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Read()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Update()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
+
+
 
