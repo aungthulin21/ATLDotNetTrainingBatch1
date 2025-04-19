87b7af590eb23ec0701b2e3609784468187bc8a4
Aung Thu Lin <aungthulin.atl.personal@gmail.com>
2025-04-19 11:40:33 +0630

    code update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
-Console.WriteLine("Hee Hee");
-Console.ReadLine();
+﻿//if and if else
+//string test = "D";
+//if (test == "A")
+//{
+//    Console.WriteLine(test);
+//}
+//else if (test == "B")
+//{
+//    Console.WriteLine(test);
+//}
+//else if (test == "C")
+//{
+//    Console.WriteLine(test);
+//}
+//else
+//{
+//    Console.WriteLine("Invalid input");
+//}
+
+//switch
+//Console.WriteLine("Enter string");
+//string test=Console.ReadLine();
+//switch (test)
+//{
+//    case "A":Console.WriteLine("Case A");
+//        break;
+//    case "B":
+//        Console.WriteLine("Case B");
+//        break;
+//    case "C":
+//        Console.WriteLine("Case C");
+//        break;
+//    default:
+//        Console.WriteLine("Invalid input");
+//        break;
+//}
+
+
