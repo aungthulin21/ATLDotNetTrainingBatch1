63d27494abc7379be4adcf23c9f3d48c0b6534d9
Aung Thu Lin <aungthulin.atl.personal@gmail.com>
2025-04-23 19:31:48 +0630

    feat:Inventory(CRUD)

--- Program.cs
+++ Program.cs
@@ -148,8 +148,8 @@
 //    Console.WriteLine(v);
 //}
 
-//int[] str = { };
-//int result=str.FirstOrDefault();
+//string[] str = {"first", "second", "third" };
+//string result = str.FirstOrDefault();
 //Console.WriteLine(result);
 
 //file
@@ -163,6 +163,58 @@
 
 //DateTime now= DateTime.Now;
 //Console.WriteLine(now.ToString("yyyy-MM-dd"));
+
+
+
+using ATLDotNetTrainingBatch1;
+
+BeforeSystem:
+
+Console.WriteLine("Inventory Management System");
+
+Console.WriteLine("1. Create Product");
+Console.WriteLine("2. View Products");
+Console.WriteLine("3. Update Product");
+Console.WriteLine("4. Delete Product");
+Console.WriteLine("5. Exit");
+
+Console.Write("Select an option: ");
+int option = int.Parse(Console.ReadLine()!);
+
+InventoryService inventoryService = new InventoryService();
+
+switch (option)
+{
+    case 1:
+        Console.WriteLine("Create Product");
+        inventoryService.CreateProduct();
+        break;
+    case 2:
+        Console.WriteLine("View Products");
+        inventoryService.ViewProducts();
+        break;
+    case 3:
+        Console.WriteLine("Update Product");
+        inventoryService.UpdateProduct();
+        break;
+    case 4:
+        Console.WriteLine("Delete Product");
+        inventoryService.DeleteProduct();
+        break;
+    case 5:
+        Console.WriteLine("Exiting...");
+        goto Exit;
+    default:
+        Console.WriteLine("Invalid option. Please try again.");
+        break;
+}
+
+goto BeforeSystem;
+
+Exit:
+Console.ReadKey();
+
+
 
 
 
